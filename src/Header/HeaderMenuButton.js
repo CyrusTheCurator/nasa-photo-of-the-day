4263270d8fd32735f63cc263155c02868b41affc
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-02-06 11:41:02 -0800

    removed string where callback was expected on MenuButton onClick

--- src/Header/HeaderMenuButton.js
+++ src/Header/HeaderMenuButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 function HeaderMenuButton() {
   return (
-    <div id="headerMenuButton" onClick="">
+    <div id="headerMenuButton">
       <div className="bar1"></div>
       <div className="bar2"></div>
       <div className="bar3"></div>
